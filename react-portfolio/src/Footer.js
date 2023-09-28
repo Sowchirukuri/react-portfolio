c2e0f9b22eb21c7f51273406643372b0a73095ec
Sowchirukuri <lakshmisowmya.chirukuri@gmail.com>
2023-09-27 19:17:57 -0500

    add socials

---
+++ react-portfolio/src/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-content">
+        <div className="social-links">
+          <a href="https://github.com/Sowchirukuri" target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>
+          <a href="https://www.linkedin.com/in/sri-lakshmi-sowmya-mallipudi-b86a43123/" target="_blank" rel="noopener noreferrer">
+            LinkedIn
+          </a>
+        </div>
+        <p>&copy; {new Date().getFullYear()} Your Name</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
